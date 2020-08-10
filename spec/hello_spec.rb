2c2cd80c47fe2debc53b599d5ff2ae468c921f4d
tanakaryo <ch5cxcxxh@gmail.com>
2020-08-10 21:10:20 +0900

    rspec use context before

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -1,7 +1,21 @@
 require 'spec_helper'
 
-RSpec.describe '四則演算' do
-    it '1 + 1 は２になること' do
-        expect(1 + 1).to eq 2
+RSpec.describe 'User' do
+    describe '#greet' do
+        before do
+            @params = { name: 'たろう'}
+        end
+        context '12歳以上の場合' do
+            it '平仮名で答えること' do
+                user = User.new(@params.merge(age: 12))
+                expect(user.greet).to eq 'ぼくはたろうだよ'
+            end
+        end
+        context '13歳以上の場合' do
+            it '漢字で答えること' do
+                user = User.new(@params.merge(age: 13))
+                expect(user.greet).to eq '僕はたろうです'
+            end
+        end
     end
 end
