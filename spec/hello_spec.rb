3d984cbd988231dd9a3d74e8b62b05b91c52e59f
tanakaryo <ch5cxcxxh@gmail.com>
2020-08-10 22:07:57 +0900

    use shared_example it_behaves_like

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -2,16 +2,31 @@ require 'spec_helper'
 
 RSpec.describe 'User' do
     describe '#greet' do
-        let(:user) {User.new(params)}
-        let(:params) {{name: 'たろう', age: age }}
+        let(:user) {User.new(name: 'たろう', age: age)}
         subject { user.greet }
-        context '12歳以上の場合' do
-            let(:age) {12}
-            it {is_expected.to eq 'ぼくはたろうだよ'}
+
+        shared_examples '子供の挨拶' do
+            it { is_expected.to eq 'ぼくはたろうだよ'}
         end
-        context '13歳以上の場合' do
-            let(:age) {13}
+        context '0歳の場合' do
+            let(:age) {0}
+            it_behaves_like '子供の挨拶'
+        end
+        context '12歳の場合' do
+            let(:age) {12}
+            it_behaves_like '子供の挨拶'
+        end
+
+        shared_examples '大人の挨拶' do
             it {is_expected.to eq '僕はたろうです'}
+        end
+        context '13歳の場合' do
+            let(:age) {13}
+            it_behaves_like '大人の挨拶'
+        end
+        context '100歳の場合' do
+            let(:age) {100}
+            it_behaves_like '大人の挨拶'
         end
     end
 end
